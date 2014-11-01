770f5a0c195abb03729c0e7d8f656ce61a2a97d0
PauloRobertoMoraesCosta <paulormc_15@hotmail.com>
2014-11-01 12:19:10 -0200

    Abrindo em apenas uma tela, porém não está acessando o Hibernate

--- src/Controller/AppPatrimonioTotal.java
+++ src/Controller/AppPatrimonioTotal.java
@@ -1,6 +1,7 @@
 package Controller;
 
 import View.JFrameLogin;
+import View.JFramePrincipal;
 import javax.swing.JOptionPane;
 
 /**
@@ -13,8 +14,10 @@ public class AppPatrimonioTotal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        JFrameLogin login = new JFrameLogin();
-        login.setVisible(true);
+        //JFrameLogin login = new JFrameLogin();
+        //login.setVisible(true);
+        JFramePrincipal principal = new JFramePrincipal();
+        principal.setVisible(true);
         
     }
     
